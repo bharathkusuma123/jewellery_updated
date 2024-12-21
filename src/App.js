97e38c25a0d55141579128b1a3d31f6dd27b4861
manju prasad <manjuprasad.4327@gmail.com>
2024-12-21 10:21:26 +0530

    Manju

--- src/App.js
+++ src/App.js
@@ -1,9 +1,5 @@
 import React from "react";
-import {
-  BrowserRouter as Router,
-  Route,
-  Routes,
-} from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import './App.css';
 import Home from "./Pages/Home/Home";
 import Schemes from "./Pages/OfflineStore/Schemes/Schemes";
@@ -19,24 +15,39 @@ import Enterprise from "./Pages/Enterprise/Enterprise";
 import Jewels from "./Pages/Brands/Jewels";
 import Jewelkart from "./Pages/Brands/Jewelkart";
 
+// Firebase imports
+import { initializeApp } from "firebase/app";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAFaHhaVX5uacKkvxCGEbfVKZ_Qo2lAlh4",
+  authDomain: "sadashri-static.firebaseapp.com",
+  databaseURL: "https://sadashri-static-default-rtdb.firebaseio.com",
+  projectId: "sadashri-static",
+  storageBucket: "sadashri-static.firebasestorage.app",
+  messagingSenderId: "516977931835",
+  appId: "1:516977931835:web:95564175a57d44a9f08e1e"
+};
+
+// Initialize Firebase
+initializeApp(firebaseConfig);
 
 function App() {
   return (
     <Router>
-      {/* < Header /> */}
-      < Navbar />
+      {/* <Header /> */}
+      <Navbar />
       <Routes>
         <Route path="/" exact element={<Home />} />
         <Route path="/schemes" exact element={<Schemes />} />
         {/* <Route path="/seller" exact element={<Seller />} /> */}
         {/* <Route path="/user" exact element={<User />} /> */}
-        <Route path="/contactUs" exact element={<Contact/>} />
-        <Route path="/AboutUs" exact element={<AboutUs/>} />
-        <Route path="/Brands" exact element={<Brands/>} />
-        <Route path="/sadashri-jewels" exact element={<Jewels/>} />
-        <Route path="/sadashri-jewelkart" exact element={<Jewelkart/>} />
-        <Route path="/Enterprise" exact element={<Enterprise/>} />
-
+        <Route path="/contactUs" exact element={<Contact />} />
+        <Route path="/AboutUs" exact element={<AboutUs />} />
+        <Route path="/Brands" exact element={<Brands />} />
+        <Route path="/sadashri-jewels" exact element={<Jewels />} />
+        <Route path="/sadashri-jewelkart" exact element={<Jewelkart />} />
+        <Route path="/Enterprise" exact element={<Enterprise />} />
       </Routes>
       <Footer />
     </Router>
@@ -44,3 +55,4 @@ function App() {
 }
 
 export default App;
+  
