6c26ccf68d5ecf8a644f77b714a58f11e3540c86
bharathkusuma123 <bharathkusuma4953@gmail.com>
2025-01-04 09:47:22 +0530

    bharath

--- src/Navbar/Navbar.js
+++ src/Navbar/Navbar.js
@@ -1,5 +1,145 @@
+// import React, { useState } from 'react';
+// import { NavLink, useLocation } from 'react-router-dom';
+// import './Navbar.css';
+// import mainlogo from './logo/sadashri parent logo123.jpg';
+
+// function Navbar() {
+//   const [isOpen, setIsOpen] = useState(false);
+//   const [brandsDropdownOpen, setBrandsDropdownOpen] = useState(false);
+//   const location = useLocation();
+
+//   const toggleMenu = () => {
+//     setIsOpen(!isOpen);
+//   };
+//   const closeMenu = () => {
+//     setIsOpen(false);
+//   };
+
+//   const toggleBrandsDropdown = () => {
+//     setBrandsDropdownOpen(!brandsDropdownOpen);
+//   };
+
+//   const isActive = (path) => location.pathname === path;
+
+//   return (
+//     <header className="navbar-header">
+//       <div className="navbar-logo">
+//         <img className="navbar-logo-img" src={mainlogo} alt="Jewellery Logo" />
+//       </div>
+
+//       <div
+//         className={`navbar-hamburger ${isOpen ? 'open' : ''}`}
+//         onClick={toggleMenu}
+//       >
+//         <div className="navbar-bar"></div>
+//         <div className="navbar-bar"></div>
+//         <div className="navbar-bar"></div>
+//       </div>
+
+//       <nav className={`navbar-links ${isOpen ? 'open' : ''}`}>
+//         <NavLink
+//           to="/"
+//           className={isActive('/') ? 'active' : ''}
+//           onClick={closeMenu}
+
+//         >
+//           Home
+//         </NavLink>
+//         <NavLink
+//           to="/AboutUs"
+//           className={isActive('/AboutUs') ? 'active' : ''}
+//           onClick={closeMenu}
+
+//         >
+//           Purpose
+//         </NavLink>
+//         <NavLink
+//           to="/Enterprise"
+//           className={isActive('/Enterprise') ? 'active' : ''}
+//           onClick={closeMenu}
+
+//         >
+//           Enterprise
+//         </NavLink>
+
+//         {/* <div
+//           className="navbar-dropdown"
+//           onMouseEnter={toggleBrandsDropdown}
+//           onMouseLeave={toggleBrandsDropdown}
+//         >
+//           <span className={isActive('/sadashri-jewels') || isActive('/sadashri-jewelkart') ? 'active' : ''}>
+//             Brands
+//           </span>
+//           {brandsDropdownOpen && (
+//             <div className="navbar-dropdown-content">
+//               <NavLink
+//                 to="/sadashri-jewels"
+//                 className={isActive('/sadashri-jewels') ? 'active' : ''}
+//               >
+//                 Sadashri Jewels-Our Store
+//               </NavLink>
+//               <NavLink
+//                 to="/sadashri-jewelkart"
+//                 className={isActive('/sadashri-jewelkart') ? 'active' : ''}
+//               >
+//                 Sadashri Jewelkart-Online Platform
+//               </NavLink>
+//             </div>
+//           )}
+//         </div> */}
+//         <div
+//   className={`navbar-dropdown ${isActive('/sadashri-jewels') || isActive('/sadashri-jewelkart') ? 'active' : ''}`}
+//   onMouseEnter={toggleBrandsDropdown}
+//   onMouseLeave={toggleBrandsDropdown}
+// >
+//   <span>
+//     Brands
+//   </span>
+//   {brandsDropdownOpen && (
+//     <div className="navbar-dropdown-content">
+//       <NavLink
+//         to="/sadashri-jewels"
+//         className={isActive('/sadashri-jewels') ? 'active' : ''}
+//         onClick={closeMenu}
+
+//       >
+//         Sadashri Jewels-Our Store
+//       </NavLink>
+//       <NavLink
+//         to="/sadashri-jewelkart"
+//         className={isActive('/sadashri-jewelkart') ? 'active' : ''}
+//         onClick={closeMenu}
+
+//       >
+//         Sadashri Jewelkart-Online Platform
+//       </NavLink>
+//     </div>
+//   )}
+// </div>
+
+
+//         <NavLink
+//           to="/contactUs"
+//           className={isActive('/contactUs') ? 'active' : ''}
+//           onClick={closeMenu}
+
+//         >
+//           Contact Us
+//         </NavLink>
+//       </nav>
+//     </header>
+//   );
+// }
+
+// export default Navbar;
+
+
+
+
+
+
 import React, { useState } from 'react';
-import { NavLink, useLocation } from 'react-router-dom';
+import { useLocation } from 'react-router-dom';
 import './Navbar.css';
 import mainlogo from './logo/sadashri parent logo123.jpg';
 
@@ -37,95 +177,61 @@ function Navbar() {
       </div>
 
       <nav className={`navbar-links ${isOpen ? 'open' : ''}`}>
-        <NavLink
-          to="/"
+        <a
+          href="/"
           className={isActive('/') ? 'active' : ''}
           onClick={closeMenu}
-
         >
           Home
-        </NavLink>
-        <NavLink
-          to="/AboutUs"
+        </a>
+        <a
+          href="/AboutUs"
           className={isActive('/AboutUs') ? 'active' : ''}
           onClick={closeMenu}
-
         >
           Purpose
-        </NavLink>
-        <NavLink
-          to="/Enterprise"
+        </a>
+        <a
+          href="/Enterprise"
           className={isActive('/Enterprise') ? 'active' : ''}
           onClick={closeMenu}
-
         >
           Enterprise
-        </NavLink>
+        </a>
 
-        {/* <div
-          className="navbar-dropdown"
+        <div
+          className={`navbar-dropdown ${isActive('/sadashri-jewels') || isActive('/sadashri-jewelkart') ? 'active' : ''}`}
           onMouseEnter={toggleBrandsDropdown}
           onMouseLeave={toggleBrandsDropdown}
         >
-          <span className={isActive('/sadashri-jewels') || isActive('/sadashri-jewelkart') ? 'active' : ''}>
-            Brands
-          </span>
+          <span>Brands</span>
           {brandsDropdownOpen && (
             <div className="navbar-dropdown-content">
-              <NavLink
-                to="/sadashri-jewels"
+              <a
+                href="/sadashri-jewels"
                 className={isActive('/sadashri-jewels') ? 'active' : ''}
+                onClick={closeMenu}
               >
                 Sadashri Jewels-Our Store
-              </NavLink>
-              <NavLink
-                to="/sadashri-jewelkart"
+              </a>
+              <a
+                href="/sadashri-jewelkart"
                 className={isActive('/sadashri-jewelkart') ? 'active' : ''}
+                onClick={closeMenu}
               >
                 Sadashri Jewelkart-Online Platform
-              </NavLink>
+              </a>
             </div>
           )}
-        </div> */}
-        <div
-  className={`navbar-dropdown ${isActive('/sadashri-jewels') || isActive('/sadashri-jewelkart') ? 'active' : ''}`}
-  onMouseEnter={toggleBrandsDropdown}
-  onMouseLeave={toggleBrandsDropdown}
->
-  <span>
-    Brands
-  </span>
-  {brandsDropdownOpen && (
-    <div className="navbar-dropdown-content">
-      <NavLink
-        to="/sadashri-jewels"
-        className={isActive('/sadashri-jewels') ? 'active' : ''}
-        onClick={closeMenu}
+        </div>
 
-      >
-        Sadashri Jewels-Our Store
-      </NavLink>
-      <NavLink
-        to="/sadashri-jewelkart"
-        className={isActive('/sadashri-jewelkart') ? 'active' : ''}
-        onClick={closeMenu}
-
-      >
-        Sadashri Jewelkart-Online Platform
-      </NavLink>
-    </div>
-  )}
-</div>
-
-
-        <NavLink
-          to="/contactUs"
+        <a
+          href="/contactUs"
           className={isActive('/contactUs') ? 'active' : ''}
           onClick={closeMenu}
-
         >
           Contact Us
-        </NavLink>
+        </a>
       </nav>
     </header>
   );
