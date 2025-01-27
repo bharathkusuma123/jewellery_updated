2ed82b30c77136805d4cf40be8fcdf19031f620a
bharathkusuma123 <bharathkusuma4953@gmail.com>
2025-01-27 10:00:36 +0530

    bharath

--- src/websiteName.js
+++ src/websiteName.js
@@ -1,3 +1,25 @@
+// import React from 'react';
+// import { Helmet } from 'react-helmet';
+
+// const WebsiteName = () => {
+//   return (
+//     <>
+//       <Helmet>
+//         <title>Sadashri Jewels | Exquisite Jewelry & Engagement Rings</title>
+//         <meta name="description" content="Sadashri Jewels offers exquisite engagement rings and fine jewelry crafted with elegance and precision." />
+//         <meta name="keywords" content="jewelry, engagement rings, Sadashri Jewels" />
+//         {/* Add other meta tags if needed */}
+//       </Helmet>
+//       {/* Add your JSX content here */}
+//     </>
+//   );
+// };
+
+// export default WebsiteName;
+
+
+
+
 import React from 'react';
 import { Helmet } from 'react-helmet';
 
@@ -5,9 +27,15 @@ const WebsiteName = () => {
   return (
     <>
       <Helmet>
-        <title>Sadashri Jewels | Exquisite Jewelry & Engagement Rings</title>
-        <meta name="description" content="Sadashri Jewels offers exquisite engagement rings and fine jewelry crafted with elegance and precision." />
-        <meta name="keywords" content="jewelry, engagement rings, Sadashri Jewels" />
+        <title>Sadashri Jewels | Timeless Creations, Lasting Impact</title>
+        <meta
+          name="description"
+          content="Discover Sadashri Jewels, where timeless craftsmanship meets innovative design. Explore our exquisite collection of fine jewelry and engagement rings that inspire elegance and sustainability."
+        />
+        <meta
+          name="keywords"
+          content="Sadashri Jewels, fine jewelry, engagement rings, sustainable jewelry, timeless craftsmanship"
+        />
         {/* Add other meta tags if needed */}
       </Helmet>
       {/* Add your JSX content here */}
